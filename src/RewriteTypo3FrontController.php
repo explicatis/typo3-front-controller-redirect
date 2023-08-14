276731405e73b1c8f1e3d9953a0fa63e459b32b6
Jörn Wagner <joern.wagner@explicatis.com>
2023-08-14 12:01:10 +0200

    Try a much simpler approach that does not rely on TYPO3 commands

---
+++ src/RewriteTypo3FrontController.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Explicatis\Typo3FrontControllerRedirect;
+
+class RewriteTypo3FrontController
+{
+    public static function rewrite(): void
+    {
+        copy(__DIR__ . '/../../new_index.php', 'public/index.php');
+    }
+}
